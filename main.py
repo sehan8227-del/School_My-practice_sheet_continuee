3fe05a59f2970f0fed24bbb1f16c84620b214415
이세한 <sehan8227@gmail.com>
2025-12-29 15:20:41 +0900

    바꾼 파일의 내용을 써넣었으며,웹사이트의 빈창을 열게하는 코드를 적어넣음.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import tkinter as tk
 root = tk.Tk()
 root.mainloop()
-
+#1.impor와 as는 같이 오는 문법이며,tkinter를 tk로 명하겠다는뜻이며,
+#2.root를 써서 tk라 명칭한것을 넣고, TK는 tkinter어플리케이션의 빈창을 생성
+#3.root과mainloop를 넣는데,mainloop의 의미는 어플리케이션이 실행되는 동안 창이
+# 닫히지 않도록 하는 역할을 한다. 
