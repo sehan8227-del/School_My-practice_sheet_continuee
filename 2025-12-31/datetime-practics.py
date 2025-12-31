4a064b9a3f50af8e292236e13b2c7c8b5765bd2b
이세한 <sehan8227@gmail.com>
2025-12-31 16:11:08 +0900

    datetime을 연습하며 손에 익기위해  import date와 import calendar을 몇번 기입하여 넣어둠

--- 2025-12-31/datetime-practics.py
+++ 2025-12-31/datetime-practics.py
@@ -25,3 +25,7 @@ print(f"{year}년 {month}월")
 # 2.날짜 옆에 빈자리를 넣는다.
 for day in range(1, days + 1):
     print(f"{month}월 {day}일  [포차코 자리]")
+
+for day in range(1, days + 1):
+    print(f"{month}월 {day}일 [포차코 자리]")
+
