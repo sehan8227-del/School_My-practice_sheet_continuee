949e960fba3151c5067a5eebdea1a6ab84400129
이세한 <sehan8227@gmail.com>
2026-01-01 16:04:09 +0900

    datetime을 이용한 달력을 만들어보고자 연습을 했지만 감이 안잡혀 그저 그해에 해당되는 년과 해당되는 달의 1~부터끝자리까지 나오는 월일만 뽑을수있게 손만 익힘.

--- 2025-12-31/datetime-practics.py
+++ 2025-12-31/datetime-practics.py
@@ -5,7 +5,58 @@ import calendar
 import datetime
 import calendar
 
+import datetime
+import calendar
+
+import datetime
+import calendar
+
+import datetime
+import calendar
+
 now = datetime.datetime.now()
+now = datetime.datetime.now()
+now = datetime.datetime.now()
+now = datetime.datetime.now()
+now = datetime.datetime.now()
+now = datetime.datetime.now()
+now = datetime.datetime.now()
+year = now.year 년
+year = now.year
+year = now.year
+year = now.year
+month = now.month 월
+month = now.month
+month = now.month
+month = now.month
+days = calendar.monthrange(year, month)
+days = calendar.monthrange(year, month)
+days = calendar.monthrange(year, month)
+days = calendar.monthrange(year, month)
+print(f"{year}년 {month}월")
+print(f"{year}년 {month}월")
+print(f"{year}년 {month}월")
+print(f"{year}년 {month}월")
+print(f"{year}년 {month}월")
+
+for day in range(1,days + 1):
+    print(f"{year}월 {day}일 [포차코 자리]")
+    
+for day in range(1, days + 1):
+    print(f"{year}월 {days}일 [포차코 자리]")
+for day in range(1, days +1):
+    print(f"{year}월 {days}일 [포차코 자리]")
+for day in range(1, days + 1):
+    print(f"{year}월 {days}일 [포차코 자리]")
+for day in range(1, days +1):
+    print(f"{year}월 {days}일 [포차코 자리]")
+#연습하다 궁금한것 for문을 왜 쓰지?굳이 쓸 이유가 있나?
+#print중 f는 뭐지? 뭐길래 자세히 기입안해도 인식되서 들어가는 걸까?
+
+
+
+
+
 year = now.year
 month = now.month
 month = now.month
