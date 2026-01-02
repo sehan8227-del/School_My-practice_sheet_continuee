8bb7f2c9e106b5410e753e55479ddfc500fba867
이세한 <sehan8227@gmail.com>
2026-01-02 12:47:43 +0900

    .\practics-lab-bench.py파일명으로 파일을 만들고 예제를 통한 콘텐츠를 기록 추가 하는 실험을 진행

---
+++ SchoolPractics-my-Assessment/reading_app_practics/practics-lab-bench.py
@@ -0,0 +1,13 @@
+#실험대
+contents = []
+contents = {
+    "title": "DeathNote",
+    "category": "contents",
+    "format": "comics",
+    "memo": "humanity chaos fall"
+}
+contents = [contents.append(contents)]
+# - append로 리스트에 넣기를 시도 적용이 안됨.왜지?
+#혹시나 해서 [contents.append]의 .append뒤 (contents)를 집어넣어도 안됨.이해가 안가 막힘.
+#GPT에게 말해 본 결과 그냥
+#contents.append(content) 이 문법이 맞았음.너무 어렵게 및 꼬여서 생각함.
