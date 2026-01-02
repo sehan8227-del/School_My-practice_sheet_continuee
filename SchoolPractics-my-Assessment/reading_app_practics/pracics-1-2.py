0812ed5a4eb7f80178b595ca0ccbb787990009e3
이세한 <sehan8227@gmail.com>
2026-01-02 12:25:08 +0900

    처음 연습시 콘텐츠 데이터를 담을 빈 리스트를 만들며 리스트가 어떻게 만들어지는지 테스트하고 현재는 새로운 도서 콘텐츠를 기록 추가 해보는 작업 중

---
+++ SchoolPractics-my-Assessment/reading_app_practics/pracics-1-2.py
@@ -0,0 +1,94 @@
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = []
+contents = {
+    "title": "데스노트",
+    "category": "콘텐츠",
+    "format": "만화",
+    "memo": "스토리 구조 관찰"
+}
+# 한번 영어로 써볼까?나름 될것 같은데?
+contents = {
+    "title": "DeathNote",
+    "category": "contents",
+    "format": "comics",
+    "memo": "humenity chaos fall"
+}
+contents = {
+    "title": "DeathNote",
+    "category": "contents",
+    "format": "comics",
+    "memo": "humenity chaos fall"
+}
+contents = {
+    "title": "DeathNote",
+    "category": "comics",
+    "format": "comics",
+    "memo": "humentiy chaos fall"
+}
+contents = {
+    "title": "DeathNote"
+    "category": "comics",
+    "format": "comics",
+    "memo": "humenity chaos fall"
+}
+#카오스의 철자가 이게 맞던가?
+#잘못썼음 category에 컨텐츠가 들어가야하는데 comics를 넣음
+#객체안에 문자열을 넣을때 :으로 닫고 문자열 넣고 다른 속성의 문자열로 다시 넘어갈때 , 를 자꾸 뺴먹음.
+comtents = {
+    "title": "DeathNote",
+    "category": "contents",
+    "format": "comics",
+    "memo": "humenity chaos fall"
+}
+#contents를 기입할때 con중com이 철자 오류가 많이 남.
+contents = {
+    "title": "DeathNote",
+    "category": "contents",
+    "format": "comics",
+    "memo": "humenity chaos fall"
+}
+contents = {
+    "title": "DeathNote",
+    "category": "contents",
+    "format": "comics",
+    "memo": "humenity chaos fall"
+}
+contents = {
+    "title": "DeathNote",
+    "category": "contents",
+    "format": "comics",
+    "memo": "humenity chaos fall"
+}
+contents = {
+    "title": "DeathNote",
+    "category": "contents",
+    "format": "comics",
+    "memo": "humenity chaos fall"
+}
+#타자가 느리긴 하지만 천천히 가능 해지고 있는 듯함.
+#어제 새벽까지 버텨서 인지 눈이 흐릿하여 집중이 잘 안되긴하네.역시 잠을 잘 자야겠다
+contents = {
+    "title": "DeathNote",
+    "category": "contents",
+    "format": "comics",
+    "memo": "humenity chaos fall"
+}
