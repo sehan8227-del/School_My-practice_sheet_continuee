88ac9821d22072d193b880f8006bfb14e63d5cd5
이세한 <sehan8227@gmail.com>
2026-01-11 20:42:37 +0900

    2026-01-11 오후 7시40분경 반복훈련 A파트 종료.

---
+++ 2026-01-08/Repetition-Drill-A-06.py
@@ -0,0 +1,646 @@
+original = "공욱재"
+copy = original
+print(copy)
+
+# 어제(26-01-07날) 브랜치에 대해 개념적으로 알아보기위해 연습하다 다시 연습 해야 하지만 금일(26-01-08)은 브랜치보단 반복연습이 더 필요하다는 판단 하에 연습 
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+#현재 졸리기에 잠도 꺨겸 약간 뇌빼고 그저 손에 익히기위해 연습중
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+#original 에 문자열"이세한"을 부여했고,copy라는 변수에 original를 덧 입혀줬으니 print에 copy를 넣어주면 original에 부여해줬던 문자열 "이세한"이 나타나는구나  
+
+original = "이세한"
+copy = original
+print(copy)
+
+# 계속 반독수리 타법이 되어 고칠려고 약지와 새끼를 움직이지만 습관이 굳혀져있어 상당히 어려움.
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "11"
+copy = original
+print(copy)
+
+original = "11"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "기러기"
+copy = original
+print(copy)
+
+original = "스위스"
+copy = original
+print(copy)
+
+original = "스위스"
+copy = original
+print(copy)
+
+original = "스위스"
+copy = original
+print(copy)
+
+original = "스위스"
+copy = original
+print(copy)
+
+original = "스위스"
+copy = original
+print(copy)
+
+original = "스위스"
+copy = original
+print(copy)
+
+original = "스위스"
+copy = original
+print(copy)
+
+original = "스위스"
+copy = original
+print(copy)
+
+original = "스위스"
+copy = original
+print(copy)
+
+original = "스위스"
+copy = original
+print(copy)
+
+original = "스위스"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+original = "이세한"
+copy = original
+print(copy)
+
+#나름 뇌빼고 하니 손 연습은 잘 익혀지는것 같은데?근데 좀아쉬운건 손은 익혀지는것 같은데 문법이 머리 속에 잘 안들어오는게 문제긴 하다.
+
+
+name = input("공욱재")
+print(name)
+
+name = input("박희망")
+print(name)
+
+name = input("박희망")
+print(name)
+
+name = input("박희망")
+print(name)
+
+name = input("박희망")
+print(name)
+
+name = input("이세한")
+print(name)
+
+# 변수를 저장한다? 저장? 변수는 바뀌는데 바뀌는걸 저장한다라....
+#흠...
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("송나영")
+print(name)
+
+name = input("송나영")
+print(name)
+
+name = input("송나영")
+print(name)
+
+name = input("송나영")
+print(name)
+
+name = input("송나영")
+print(name)
+
+name = input("송나영")
+print(name)
+
+name = input("송나영")
+print(name)
+
+name = input("송나영")
+print(name)
+
+name = input("송나영")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+# 연습하는 와중에도 딱히 와닿지가 않네?저장이라 저장 input에 저장이된다한들 이게 어디에 써먹게 되는건질 모르겠네
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+
+name = input("이세한")
+print(name)
+# input안에 이세한이라는 문자열을 집어 넣고 name인 변수로 저장한 다음 print로 name을 출력시
+# 이세한 이라는 문자열이 나옴.
+name = input("이세한")
+age = input("이세한")
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+#26-01-09 오후 9시40분 뇌빼고 반복연습을 하는 도중에 시야가 계속 두갈래로 보이고 동공이 풀린것같아 훈련종료
+
+
+#26-01-11 반복훈련 오후 7시 30분 시작
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+name = input("이세한")
+age = input(31)
+print(name)
+print(age)
+
+#2026-01-11 오후 7시40분경 반복훈련 A파트 종료.
+
+
+
