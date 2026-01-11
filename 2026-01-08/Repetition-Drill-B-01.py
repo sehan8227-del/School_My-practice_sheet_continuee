ac4a02442bebc1ca5b0bf5ce8c769f9bf954672e
이세한 <sehan8227@gmail.com>
2026-01-11 19:43:43 +0900

    2026-01-11 오후 7시 40분경 도서관에서 반복 훈련을 위해 new-item으로 \Repetition-Drill-B-01.py파일을 만들고 반복훈련에 대한 내용을 기입

---
+++ 2026-01-08/Repetition-Drill-B-01.py
@@ -0,0 +1,11 @@
+#name 이라는 변수안에 input()을 이용하여 문자열인 "이세한"과 age라는 변수 또한 input()을 이용하여 31이라는 숫자를 저장한것이다.
+
+members = []
+print(members)
+#결과 값은 []가 나온다?
+
+members = []
+print(members)
+
+members = []
+print(members)
