536cb51adb38852e6fab4fcf601d4e05485367c7
이세한 <sehan8227@gmail.com>
2026-01-11 20:02:35 +0900

    반복연습을 하면서 문득문득 쉬운것인데 그냥 넘어갈까? 생각과 컨디션조절 훈련이 아직도안되었나라는 생각이 많아짐.

--- 2026-01-08/Repetition-Drill-B-01.py
+++ 2026-01-08/Repetition-Drill-B-01.py
@@ -1,3 +1,5 @@
+# 2026-01-11 오후 7시40분쯤 반복훈련 A를 끝내고 반복훈련 B로 넘어옴. 
+
 #name 이라는 변수안에 input()을 이용하여 문자열인 "이세한"과 age라는 변수 또한 input()을 이용하여 31이라는 숫자를 저장한것이다.
 
 members = []
@@ -9,3 +11,115 @@ print(members)
 
 members = []
 print(members)
+
+members = []
+print(members)
+
+# 저장하고 실행시켜보니 결과값이 [] 이 나오네
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+#이게 리스트안에 넣는 방식을 먼저 알기위해 이걸 먼저 쓰는건가?
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = [] #아하..계속 손이 꼬이네
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = [] #쉬프트를 무의식으로 이용하여 { } 중괄호를 사용하게되네
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+#계속하다보니 간단한거라도 그냥 넘어가면 안되겠지 와 아..간단한건데 그냥 넘어갈까라는생각이 부딪히는데 계속 해보는게 맞을까?란 생각이 드네
+
+members = []
+print(members)
+
+
+members = []
+print(members)
+
+#물론 직접 치곤 있지만 타인이 본다면 ctorl+C ctorl+V처럼 보이지 않을까? 이게 잡생각일지 모르겠지만 벌써부터 살짝 피곤감이 오네...컨디션이 안좋나? 늘 컨디션 조절을 하려 노력을 하는데 뭐가 나한테 안 맞을까?뭐가 문제지?참..생각이 많아지네
+
+members = []
+print(members)
+
+members = []
+print(members)
+
+members = []
+print(members)
+
